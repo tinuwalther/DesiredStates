ecf48525614859323314d8a478a7555327218966
tinuwalther <it@martin-walther.ch>
2018-02-27 22:19:06 +0100

    Create DesiredServiceState.ps1

---
+++ DesiredServiceState.ps1
@@ -0,0 +1,28 @@
+configuration DesiredServiceState
+{
+    param(
+        [Array]$ServiceNames
+    )
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+    Node localhost
+    {
+        foreach($service in $ServiceNames)
+        {
+            $name="$($service)-ShouldBe-Automatic-and-Running"
+            Service $name
+            {
+                Name        = $service
+                StartupType = "Automatic"
+                State       = "Running"
+            } 
+        }
+    }
+}
+
+#Generate MOF-File
+#$services = @('Eventlog','MpsSvc')
+#DesiredServiceState -ServiceNames $services -OutputPath "C:\Work\DscRepo"
+
+#Start DSC Configuration manual
+#Start-DscConfiguration -Path "C:\Work\DscRepo" -Verbose -Wait -Force
