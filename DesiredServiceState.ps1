9b8035a57ebafa9a86c643b456e81e109c998061
tinuwalther <it@martin-walther.ch>
2018-02-28 13:22:00 +0100

    Update DesiredServiceState.ps1

--- DesiredServiceState.ps1
+++ DesiredServiceState.ps1
@@ -1,28 +1,36 @@
 configuration DesiredServiceState
 {
     param(
-        [Array]$ServiceNames
+        [Array]$ServiceNames,
+        [String]$StartupType,
+        [String]$ServiceState
     )
     Import-DscResource -ModuleName PSDesiredStateConfiguration
 
     Node localhost
     {
-        foreach($service in $ServiceNames)
+        foreach($ServiceName in $ServiceNames)
         {
-            $name="$($service)-ShouldBe-Automatic-and-Running"
+            $name="$($ServiceName)-ShouldBe-Automatic-and-Running"
             Service $name
             {
-                Name        = $service
-                StartupType = "Automatic"
-                State       = "Running"
+                Name        = $ServiceName
+                StartupType = $StartupType
+                State       = $ServiceState
             } 
         }
     }
 }
 
 #Generate MOF-File
-#$services = @('Eventlog','MpsSvc','LanmanServer','Schedule','wuauserv','Netlogon','WinRM','W32Time')
+$autoservices   = @('Eventlog','MpsSvc','LanmanServer','Schedule','WinRM')
+$StartupType    = 'Automatic'
+$ServiceState   = 'Running'
 #DesiredServiceState -ServiceNames $services -OutputPath "C:\Work\DscRepo"
+
+$manualservices = @('wuauserv','Netlogon','W32Time')
+$StartupType    = 'Manual'
+$ServiceState   = 'Stopped'
 
 #Start DSC Configuration manual
 #Start-DscConfiguration -Path "C:\Work\DscRepo" -Verbose -Wait -Force
