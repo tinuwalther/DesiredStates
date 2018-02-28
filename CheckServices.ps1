5c6963e84cbbe97f9851b2e86c4f9f7bee82823a
tinuwalther <it@martin-walther.ch>
2018-02-28 13:18:04 +0100

    Create CheckServices.ps1

---
+++ CheckServices.ps1
@@ -0,0 +1,15 @@
+$services = @('Eventlog','MpsSvc','LanmanServer','Schedule','wuauserv','Netlogon','WinRM','W32Time')
+$psoServices = @()
+foreach($item in $services){
+    Get-WmiObject Win32_Service | Where-Object {$_.Name -eq $item} | Select-Object Name,DisplayName,State,StartMode,Started | % {
+        $obj = [PSCustomObject]@{
+            Name        = $_.Name
+            DisplayName = $_.DisplayName
+            State       = $_.State
+            StartMode   = $_.StartMode
+            Started     = $_.Started
+        }
+        $psoServices += $obj
+    }
+}
+$psoServices | ft -AutoSize
